cdd6b75ca4b76485a96c34eca155141958cb7645
windyChess <13764906801@163.com>
2016-08-18 11:49:53 +0800

    jscsh

---
+++ jscsh/js/basic.js
@@ -0,0 +1,25 @@
+/**
+ * Created by hfq on 2016/8/18.
+ */
+/*label语句使用*/
+//未使用label语句
+/*for(var i=0; i<10; i++){
+    for(var j=0; j<10; j++){
+        if(j == 5 && i == 5){
+            console.log('j for--'+j);
+            break;
+        }
+    }
+    console.log('i for--'+i);
+}*/
+
+//使用label语句
+start:for(var i=0; i<10; i++){
+    for(var j=0; j<10; j++){
+        if(j == 5 && i == 5){
+            console.log('j for--'+j);
+            break start;
+        }
+    }
+    console.log('i for--'+i);
+}
